94b48bfe7ab7af94094acb3aa38b081343b04b5f
zhengzhonglve <zhengzhonglve@126.com>
2020-04-05 00:25:54 +0800

    springmvc搭建

--- booksystem/src/main/java/com/zheng/book/booksystem/controller/HelloController.java
+++ booksystem/src/main/java/com/zheng/book/booksystem/controller/HelloController.java
@@ -2,11 +2,19 @@ package com.zheng.book.booksystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+	
 	@RequestMapping("/hello.do")
+	@ResponseBody
 	public String hello() {
 		return "hello";
+	}
+	
+	@RequestMapping("/index.do")
+	public String index() {
+		return "index";
 	}
 }
